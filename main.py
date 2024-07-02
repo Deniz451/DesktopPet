54047d9387dd43d0fc6e42e4de48944f9d036f5e
Denis <114061026+Deniz451@users.noreply.github.com>
2024-07-02 10:15:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -15,18 +15,19 @@ screen_height = info.current_h
 spawn_x = random.randint(0, screen_width)
 spawn_y = random.randint(0, screen_height)
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+#screen = pygame.display.set_mode((screen_width, screen_height))
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-#sprite_sheet_image = pygame.image.load('photos/sprite_sheet_cropped.png').convert_alpha()
-sprite_sheet_image = pygame.image.load('photos/sprite_sheet.png').convert_alpha()
+sprite_sheet_image_walk = pygame.image.load('sprite_sheet_walk.png').convert_alpha()
+sprite_sheet_image_idle = pygame.image.load('sprite_sheet_idle.png').convert_alpha()
 bg = (50, 50, 50)
 black = (0, 0, 0)
 
 hwnd = pygame.display.get_wm_info()["window"]
-win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
-                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*bg), 0, win32con.LWA_COLORKEY)
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TOOLWINDOW)
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_TOPMOST | win32con.WS_EX_NOACTIVATE)
 
 
 def get_image(sheet, frame, row, width, height, scale, color):
@@ -39,18 +40,21 @@ def get_image(sheet, frame, row, width, height, scale, color):
 
 clock = pygame.time.Clock()
 
+sprite_width = 160 * 0.5
+sprite_height = 360 * 0.5
 x, y = spawn_x, spawn_y
 move_speed = 5
 column = 0
 row = 0
-column_count = sprite_sheet_image.get_width() // 160
+column_count = sprite_sheet_image_walk.get_width() // 160
+idle_frame_count = sprite_sheet_image_idle.get_width() // 160
 
 run = True
 while run:
 
     # Determine movement direction
     if random.randint(0, 1) == 0:
-        rand_x = random.randint(0, screen_width)
+        rand_x = random.randint(0, screen_width - int(sprite_width))
         rand_y = y
 
         if rand_x > x:
@@ -58,7 +62,7 @@ while run:
         elif rand_x < x:
             dir = 'left'
     else:
-        rand_y = random.randint(0, screen_height)
+        rand_y = random.randint(0, screen_height - int(sprite_height))
         rand_x = x 
 
         if rand_y > y:
@@ -71,10 +75,10 @@ while run:
             row = 0
         case 'left':
             row = 1
-        case 'down':
-            row = 3
         case 'top':
             row = 2
+        case 'down':
+            row = 3
         
     while (x, y) != (rand_x, rand_y):
         for event in pygame.event.get():
@@ -92,8 +96,7 @@ while run:
 
         
         screen.fill(bg)
-        frame_image = get_image(sprite_sheet_image, column, row, 160, 360, 0.5, black)
-        #frame_image = get_image(sprite_sheet_image, frame, 0, 160, 160, 0.5, black)
+        frame_image = get_image(sprite_sheet_image_walk, column, row, 160, 360, 0.5, black)
         screen.blit(frame_image, (x, y))
         pygame.display.flip()
 
@@ -101,10 +104,19 @@ while run:
 
         clock.tick(10)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
+    for i in range(6):  # 3 frames looped twice
+        screen.fill(bg)
+        idle_frame = i % idle_frame_count
+        idle_frame_image = get_image(sprite_sheet_image_idle, idle_frame, row, 160, 360, 0.5, black)
+        screen.blit(idle_frame_image, (x, y))
+        pygame.display.flip()
+
+        clock.tick(10)  # Wait 1 second between each frame
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
 
 
 pygame.quit()
